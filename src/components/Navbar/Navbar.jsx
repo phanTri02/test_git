c172db3439e47ebaf51bc95ab99be55090a86172
phanTri <phanquoctri1011@gmail.com>
2022-12-03 19:52:46 +0700

    nav bar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import '../Navbar.css'
 
-
-const Nav = () => {
+function Navbar(props) {
     return (
-        <div>Nav</div>
+        <nav className="nav">
+            <a href="" className="brand">LOGO</a>
+            <ul className="nav__menu">
+                <li className='nav__item'>
+                    <a href="" className="nav__link">Các khóa học</a>
+                </li>
+                <li className='nav__item'>
+                    <a href="" className="nav__link">Tuyển dụng</a>
+                </li>
+                <li className='nav__item'>
+                    <a href="" className="nav__link">Theo dõi</a>
+                </li>
+            </ul>
+            <div className="nav_toggler">
+                <div className="line1"></div>
+                <div className="line2"></div>
+                <div className="line3"></div>
+            </div>
+        </nav>
     );
 }
- 
-export default Nav;
+
+export default Navbar;
