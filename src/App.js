002337203bd0156bde379d1ecc00a357f1370413
phanTri <phanquoctri1011@gmail.com>
2022-12-06 10:34:40 +0700

    fixed 2nd: navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import { Homepage } from './pages';
+import Navbar from "../src/components/Navbar/Navbar"
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Homepage/>
     </div>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import './Navbar.css'
+// import React from 'react';
+// import './Navbar.css'
 
-function Navbar(props) {
-    return (
-        <div className="nav">
-            <a href="" className="brand">LOGO</a>
-            <ul className="nav__menu">
-                <li className='nav__item'>
-                    <a href="" className="nav__link">Các khóa học</a>
-                </li>
-                <li className='nav__item'>
-                    <a href="" className="nav__link">Tuyển dụng</a>
-                </li>
-                <li className='nav__item'>
-                    <a href="" className="nav__link">Theo dõi</a>
-                </li>
-            </ul>
-            <div className="nav_toggler">
-                <div className="line1"></div>
-                <div className="line2"></div>
-                <div className="line3"></div>
-            </div>
-        </div>
-    );
-}
+// function Navbar(props) {
+//     return (
+//         <div className="nav__content">
+//             <a href="" className="brand">LOGO</a>
+//             <ul className="nav__menu">
+//                 <li className='nav__item'>
+//                     <a href="" className="nav__link">Các khóa học</a>
+//                 </li>
+//                 <li className='nav__item'>
+//                     <a href="" className="nav__link">Tuyển dụng</a>
+//                 </li>
+//                 <li className='nav__item'>
+//                     <a href="" className="nav__link">Theo dõi</a>
+//                 </li>
+//             </ul>
+//             <div className="nav_toggler">
+//                 <div className="line1"></div>
+//                 <div className="line2"></div>
+//                 <div className="line3"></div>
+//             </div>
+//         </div>
+//     );
+// }
 
-export default Navbar;
+// export default Navbar;
